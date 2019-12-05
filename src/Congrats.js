261b5ff2f6a87912bb73282d0082bc02cc1b3496
Edwin <Edwin.Martinez@people10.com>
2019-12-04 19:19:54 -0500

    Add Congrats component and test utils

---
+++ src/Congrats.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default ({success}) => {
+  if(success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return <div data-test="component-congrats"></div>
+  }
+
+}
