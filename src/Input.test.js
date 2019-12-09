e90c1237c068f0f3257fbdd1d389ee809be086f0
Edwin <Edwin.Martinez@people10.com>
2019-12-09 18:45:51 -0500

    Write tests for Input success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -38,14 +38,23 @@ describe("render", () => {
   })
 
   describe("word has been guessed", ()=> {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: true}
+      wrapper = setup(initialState);
+    })
     test("renders component without error", ()=> {
-
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     })
     test("does Not render input box", ()=> {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
 
     })
     test("does Not render submit button", ()=> {
-
+      const submitButton = findByTestAttr(wrapper, 'submit-button')
+      expect(submitButton.length).toBe(0);
     })
   })
 })
