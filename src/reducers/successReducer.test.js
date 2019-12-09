5c9722a133e735ffd5031da466ffca05e7f5593e
Edwin <Edwin.Martinez@people10.com>
2019-12-09 16:14:28 -0500

    Write code and tests for sucessReducer

---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,12 @@
+import successReducer from './successReducer';
+import {actionTypes} from '../actions';
+
+test("returns default inital state 'false' when no action is passed", () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+})
+
+test("returns state of true upon receiving an action of type 'CORRECT_GUESS'", () => {
+  const newState = successReducer(undefined, {type: actionTypes.CORRECT_GUESS})
+  expect(newState).toBe(true)
+})
